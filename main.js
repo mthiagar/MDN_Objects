2e942e8f746aa01ac46f302574e6e86f07a511c8
Madhuran Thiagarajah <madhuran.v@gmail.com>
2022-08-21 14:01:44 -0400

    Add user controlled evil circle

--- main.js
+++ main.js
@@ -79,6 +79,71 @@ class Ball extends Shape{
 }
 };
 
+class EvilCircle extends Shape{
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = 'white';
+    this.size = 10;
+
+  window.addEventListener('keydown', (e) => {
+    switch(e.key) {
+      case 'a':
+        this.x -=this.velX;
+        break;
+      case 'd':
+        this.x +=this.velX;
+        break;
+      case 'w':
+        this.y -=this.velY;
+        break;
+      case 's':
+        this.y +=this.velY;
+        break;
+    }
+  })
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  };
+
+  checkBounds() {
+    if ((this.x + this.size) >= width) {
+      this.x = width - this.size;
+    }
+
+    if ((this.x - this.size) <= 0) {
+      this.x = 0 + this.size;
+    }
+
+    if ((this.y + this.size) >= height) {
+      this.y = width - this.size;
+    }
+
+    if ((this.y - this.size) <= 0) {
+      this.y = 0 + this.size;
+    }
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + ball.size) {
+        ball.exists = false;
+      }
+    }
+  }
+}
+};
+
 const balls = [];
 
 while (balls.length < 25) {
@@ -95,14 +160,21 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+const evil_circle = new EvilCircle(width/2, height/2);
+
 function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, width, height);
-
+  
   for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
+    if (ball.exists){
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+      evil_circle.draw();
+      evil_circle.checkBounds();
+      evil_circle.collisionDetect();
+    }
   }
 
   requestAnimationFrame(loop);
